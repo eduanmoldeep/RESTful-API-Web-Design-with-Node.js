bb7fee5319909868914f73080bdb5bfc63d7ba0d
Anmoldeep Singh Arora <31613431+eduanmoldeep@users.noreply.github.com>
2024-05-06 09:20:08 +0530

    Debugged file 10-file-summary-destructure.js
     - added Missing Param 'utf-8' since it was returning binary buffer
     - Updated File name from '7-*' to '10-*' in code since there was no file and execution was failing

--- ch3/10-file-summary-destructure.js
+++ ch3/10-file-summary-destructure.js
@@ -1,8 +1,8 @@
 import { readFile } from 'node:fs/promises'
 
 async function readFileContent(filePath) {
-  const text = await readFile(filePath)
+  const text = await readFile(filePath,'utf-8')
   console.log(text)
 }
 
-readFileContent('./7-file-summary-destructure.js')
+readFileContent('./10-file-summary-destructure.js')
